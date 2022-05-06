c210f8a2cac6725fc8e37e678c9ef339169ec905
Caio Martins <dev.caiomartins@gmail.com>
2022-05-06 15:39:29 +0100

    upd: using PORT variable

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,4 +7,6 @@ app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 app.use(routes);
 
-app.listen(3333, () => console.log(`Listening at: localhost:3333`));
+app.listen(process.env.PORT || 3333, () =>
+  console.log(`Listening at: localhost:3333`)
+);
